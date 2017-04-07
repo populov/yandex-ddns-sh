1d04020536eb70d897a42168140c949c609ebbdf
Serge Populov <populov@gmail.com>
2017-04-07 12:03:43 +0500

    yapdate.sh uses Yandex PDD API v2

--- yapdate.sh
+++ yapdate.sh
@@ -20,7 +20,9 @@ if [ "$current_ip" != "$cache_ip" ]
 then
   echo "Updating Yandex DNS with" $current_ip
   echo `date`  "Updating with IP" $current_ip >> /usr/logs/yandex-dns-update.log
-  wget --no-check-certificate -qO- "https://pddimp.yandex.ru/nsapi/edit_a_record.xml?token=${token}&domain=${domain}&subdomain=${subdomain}&record_id=${record_id}&ttl=${ttl}&content=${current_ip}"
+  wget --no-check-certificate --header="PddToken: ${token}" \
+       --post-data "&domain=${domain}&subdomain=${subdomain}&record_id=${record_id}&ttl=${ttl}&content=${current_ip}" \
+       -qO- "https://pddimp.yandex.ru/api2/admin/dns/edit"
   rm -f $cache_file
   echo $current_ip > $cache_file
 fi
